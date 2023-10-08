60674cb851bfe050d94e31fb57dcae8ee6d2a6c3
nishida-sugiura <108458541+nishida-sugiura@users.noreply.github.com>
2023-10-08 20:50:06 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -25,21 +25,6 @@ function initializeLiff(liffId) {
         })
         .then(() => {
 
-       liff.scanCode().then(result => {
-            const stringifiedResult = result.value;
-            liff.sendMessages([{
-                'type': 'text',
-                'text': stringifiedResult
-            }]).then(() => {
-                liff.closeWindow();
-            }).catch((error) => {
-                window.alert('Error sending message: ' + error);
-            });
-        }).catch(err => {
-            window.alert('scanCode failed!');
-        });
-
-
             
           // start to use LIFF's api
           initializeApp();
