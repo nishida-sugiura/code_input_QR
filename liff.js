fcf7227fdca506dc0c9b8696be9d31803d0fa108
nishida-sugiura <108458541+nishida-sugiura@users.noreply.github.com>
2023-10-08 19:54:31 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,3 +1,17 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function () {
 
     var liffId = "1657198553-M8zND8nk";
@@ -10,6 +24,23 @@ function initializeLiff(liffId) {
             liffId: liffId
         })
         .then(() => {
+
+       liff.scanCode().then(result => {
+            const stringifiedResult = result.value;
+            liff.sendMessages([{
+                'type': 'text',
+                'text': stringifiedResult
+            }]).then(() => {
+                liff.closeWindow();
+            }).catch((error) => {
+                window.alert('Error sending message: ' + error);
+            });
+        }).catch(err => {
+            window.alert('scanCode failed!');
+        });
+
+
+            
           // start to use LIFF's api
           initializeApp();
         })
@@ -34,3 +65,30 @@ function sendText(text) {
         window.alert('Failed to send message ' + error);
     });
 }
+
+
+
+
+
+
+$('#qr-button').click(function() {
+	// LIFF の QR コード読み取り機能呼び出し
+	liff.scanCode().then(function(result) {
+		$.post(result.value, { userId: userId }, function(data) {
+			alert(data.addPoint + ' ポイントを獲得しました！');
+			$('#point').text(data.currentPoint + data.addPoint + ' ポイント');
+		}).catch(function(err) {
+			console.log(err);
+		});
+	});
+});
+
+
+
+
+
+
+
+
+
+
