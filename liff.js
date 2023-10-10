2cbba9f768c3b37629e83ee18ceee2d80475e10e
nishida-sugiura <108458541+nishida-sugiura@users.noreply.github.com>
2023-10-10 19:48:49 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,71 +1,54 @@
+
+
+
 $(document).ready(function () {
+    var liffId = '2001059289-X2m74PYZ';
+    var userId; // ユーザーIDを格納する変数
 
-    var liffId = "2001059289-X2m74PYZ";
-    initializeLiff(liffId);
-})
-
-function initializeLiff(liffId) {
-    liff
-        .init({
-            liffId: liffId
-        })
-        .then(() => {
-          // start to use LIFF's api
-          initializeApp();
-        })
-        .catch((err) => {
-            console.log('LIFF Initialization failed ', err);
-        });
-}
-
-function sendText(text) {
-    // sendMessages(text);
-// }
-
-// LINEトーク画面上でメッセージ送信
-// function sendMessages(text) {
-    liff.sendMessages([{
-        'type': 'text',
-        'text': text
-    }]).then(function () {  
-        liff.closeWindow();
-        
-    }).catch(function (error) {
-        window.alert('Failed to send message ' + error);
-    });
-}
-
-
-
-
-document.getElementById('scan-qr-button').addEventListener('click', () => {
-  liff.scanCode()
-    .then(result => {
-      const scannedCode = result.value;
-
-
-    liff.sendMessages([{
-        'type': 'text',
-        'text': scannedCode
-    }]).then(function () {  
-        liff.closeWindow();
-        
-    }).catch(function (error) {
-        window.alert('Failed to send message ' + error);
+    // LIFF 初期化
+    liff.init({
+        liffId: liffId,
+    }, function (data) {
+        console.log('liff.init 完了');
+        // プロフィール情報取得
+        liff.getProfile()
+            .then(function (userData) {
+                console.log(`liff.getProfile 完了 userId: ${userData.userId}, displayName: ${userData.displayName}`);
+                userId = userData.userId;
+                $('#name').text(userData.displayName + ' 様');
+                $('#point').text('100 ポイント');
+            })
+            .catch(function (err) {
+                console.log(`liff.getProfile 失敗 code: ${err.code}, detail: ${err.message}`);
+            });
+    }, function (err) {
+        console.log(`liff.init 失敗 ${err}`);
     });
 
-    
-    })
-    .catch(error => {
-      console.error('QRコードスキャンエラー:', error);
+    // QRコードスキャンボタンのクリックイベント
+    $('#scan-qr-button').on('click', function () {
+        liff.scanCode()
+            .then(function (result) {
+                var scannedCode = result.value;
+
+                // QRコードのスキャン結果をLINEのトークに送信
+                liff.sendMessages([{
+                    'type': 'text',
+                    'text': scannedCode
+                }])
+                .then(function () {
+                    console.log('QRコードのスキャン結果を送信しました');
+                })
+                .catch(function (error) {
+                    console.error('メッセージの送信エラー:', error);
+                });
+
+            })
+            .catch(function (error) {
+                console.error('QRコードスキャンエラー:', error);
+            });
     });
 });
-
-
-
-
-
-
 
 
 
