2a327c114b05f95d01564edaa6406f041b58dee1
nishida-sugiura <108458541+nishida-sugiura@users.noreply.github.com>
2023-10-10 20:05:24 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,6 +1,4 @@
 
-
-
 $(document).ready(function () {
     var liffId = '2001059289-X2m74PYZ';
     var userId; // ユーザーIDを格納する変数
@@ -9,18 +7,23 @@ $(document).ready(function () {
     liff.init({
         liffId: liffId,
     }, function (data) {
-        console.log('liff.init 完了');
-        // プロフィール情報取得
-        liff.getProfile()
-            .then(function (userData) {
-                console.log(`liff.getProfile 完了 userId: ${userData.userId}, displayName: ${userData.displayName}`);
-                userId = userData.userId;
-                $('#name').text(userData.displayName + ' 様');
-                $('#point').text('100 ポイント');
-            })
-            .catch(function (err) {
-                console.log(`liff.getProfile 失敗 code: ${err.code}, detail: ${err.message}`);
-            });
+        if (data.context.viewType === 'compact') {
+            // LIFFの初期化が完了したことを確認
+            console.log('LIFFの初期化が完了しました。');
+            // プロフィール情報取得
+            liff.getProfile()
+                .then(function (userData) {
+                    console.log(`liff.getProfile 完了 userId: ${userData.userId}, displayName: ${userData.displayName}`);
+                    userId = userData.userId;
+                    $('#name').text(userData.displayName + ' 様');
+                    $('#point').text('100 ポイント');
+                })
+                .catch(function (err) {
+                    console.log(`liff.getProfile 失敗 code: ${err.code}, detail: ${err.message}`);
+                });
+        } else {
+            console.log('LIFFの初期化がcompactモードでないため、処理を中止します。');
+        }
     }, function (err) {
         console.log(`liff.init 失敗 ${err}`);
     });
@@ -38,6 +41,7 @@ $(document).ready(function () {
                 }])
                 .then(function () {
                     console.log('QRコードのスキャン結果を送信しました');
+                    liff.closeWindow(); // メッセージ送信後にウィンドウを閉じる
                 })
                 .catch(function (error) {
                     console.error('メッセージの送信エラー:', error);
@@ -49,21 +53,6 @@ $(document).ready(function () {
             });
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
